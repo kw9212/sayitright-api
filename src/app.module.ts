7c6b3043fb45026b2d8830ef86fc2d100055d9a3
Keunwoo <104286190+kw9212@users.noreply.github.com>
2026-01-04 22:52:07 +0900

    chore: add healthService to providers in main modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { HealthController } from './health/health.controller';
+import { HealthService } from './health/health.service';
 
 @Module({
   imports: [
@@ -12,6 +13,6 @@ import { HealthController } from './health/health.controller';
     }),
   ],
   controllers: [AppController, HealthController],
-  providers: [AppService],
+  providers: [AppService, HealthService],
 })
 export class AppModule { }
