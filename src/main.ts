cb8606e6ca6803539f29526d6b866e2bd3d0ea93
Keunwoo <104286190+kw9212@users.noreply.github.com>
2026-01-07 11:42:01 +0900

    chore: add interceptor and exception filter for response and error handling

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { SuccessResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'fatal', 'error', 'warn', 'debug', 'verbose'],
   });
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new SuccessResponseInterceptor());
 
   const config = new DocumentBuilder()
     .setTitle('SayItRight API')
