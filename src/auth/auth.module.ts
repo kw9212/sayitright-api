a4330c059ec3ea5dcc13677c7e722ce00df21348
Keunwoo <104286190+kw9212@users.noreply.github.com>
2026-01-09 13:43:38 +0900

    chore: Redis, Cookie Parser 의존성/모듈 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
   imports: [
     UsersModule,
+    RedisModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET ?? 'dev-secret',
       signOptions: { expiresIn: '5m' },
